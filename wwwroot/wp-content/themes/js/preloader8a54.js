45ba114a120904638fef05a963350ac0f2e8e57f
tuan.cna <tuan.cna@ots/vn>
2026-06-21 18:40:31 +0700

    chore: clean up unused assets, fonts, unused scripts, and backup files

--- wwwroot/wp-content/themes/js/preloader8a54.js
+++
@@ -1,8 +0,0 @@
-// preloader
-const preloader = document.getElementById('preloader');
-
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function pre() {
-        preloader.classList.add('uk-hidden');
-    }, 500);
-});
